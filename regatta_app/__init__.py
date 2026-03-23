6cc5178bdff679a255fb619997cd1ef33e6627a2
pashkarp14 <pavel.karpov.04@mail.ru>
2026-03-23 16:00:08 +0300

    probe tree api

--- regatta_app/__init__.py
+++
@@ -1,6 +0,0 @@
-from .factory import create_app
-from .extensions import socketio
-
-app = create_app()
-
-__all__ = ["app", "create_app", "socketio"]
--- regatta_app/extensions.py
+++
@@ -1,5 +0,0 @@
-from flask_session import Session
-from flask_socketio import SocketIO
-
-session_ext = Session()
-socketio = SocketIO(cors_allowed_origins="*", manage_session=False)
